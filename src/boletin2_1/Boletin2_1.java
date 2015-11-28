69ddbd0edcc3d0fefc6b28b605a238bffbd0f0a0
AAron <AAron@Cobo-PC>
2015-11-28 14:00:43 +0100

    subir a github

---
+++ src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,14 @@
+
+package boletin2_1;
+
+public class Boletin2_1 {
+
+    public static void main(String[] args) {
+       float base = 2.0f, area; //poño f para darlle valor tipo float
+       float altura =(float)3.0; // cast
+       /*area = base*altura/2;
+        System.out.println("area = " + area);
+       */
+    }
+    
+}
